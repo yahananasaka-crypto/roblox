cf681e9e97542a516332f21ba9a6fe42966f0df3
yahanasasaka <yahananasaka@gmail.com>
2026-07-25 15:17:47 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,5 +1,5 @@
 -- === НАЛАШТУВАННЯ ===
-local discordWebhookProxy = "local discordWebhookProxy = "https://orange-water-1bd0.yahananasaka.workers.dev/api/webhooks/1516048362535784528/nbtSz5WC9weebXBSQN9JSnvyZ_lsSKMaj2JqbxmfN-Al2fbbaeD52v3GEn301d2mX3bg""
+local discordWebhookProxy = "local discordWebhookProxy = "https://orange-water-1bd0.yahananasaka.workers.dev/api/webhooks/1516048362535784528/nbtSz5WC9weebXBSQN9JSnvyZ_lsSKMaj2JqbxmfN-Al2fbbaeD52v3GEn301d2mX3bg"
 local workerUrl = "https://orange-water-1bd0.yahananasaka.workers.dev/"
 
 -- === ГЕНЕРАЦІЯ ID ===
